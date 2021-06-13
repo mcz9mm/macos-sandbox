75e145a1757187df19800d9e889bb373d7721049
mcz9mm <k.m.swim411@gmail.com>
2021-06-13 10:23:20 +0900

    Initial Commit

---
+++ sandbox/sandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  sandboxApp.swift
+//  sandbox
+//
+//  Created by Kaoru Matarai on 2021/06/13.
+//
+
+import SwiftUI
+
+@main
+struct sandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
